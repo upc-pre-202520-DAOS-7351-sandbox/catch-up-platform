48eb65fc39c25e0291a8cbdad2ae65619ca8785b
Rafael C <rafael.castro@upc.edu.pe>
2025-10-21 08:55:46 -0500

    chore: intial commit.

---
+++ src/main/java/com/acme/catchupplatform/CatchUpPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.catchupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchUpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchUpPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/catchupplatform/CatchUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.catchupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchUpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
