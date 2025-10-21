8cd1dde74f2f6a0856e84cb2f65435f5e02552e9
Rafael C <rafael.castro@upc.edu.pe>
2025-10-21 09:40:49 -0500

    feat: update main class file, create news bounded context and jpa naming strategy file.

--- src/main/java/com/acme/catchupplatform/CatchUpPlatformApplication.java
+++ src/main/java/com/acme/catchupplatform/CatchUpPlatformApplication.java
@@ -2,7 +2,19 @@ package com.acme.catchupplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * CatchUpPlatformApplication
+ *
+ * @summary
+ * The main class of the CatchUp Platform application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class CatchUpPlatformApplication {
 
